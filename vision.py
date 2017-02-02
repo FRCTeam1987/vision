0d3ea658f84ac379f6e1e7461763d970e4de5264
frc1987 <broncobots@gmail.com>
2017-02-01 20:45:19 -0600

    Update vision.py

--- vision.py
+++ vision.py
@@ -9,6 +9,7 @@ class Camera:
   def __init__(self):
     self.camera = PiCamera()
     self.camera.resolution = (640,480)
+    self.camera.framerate = 60
     self.camera.shutter_speed = 500 #works better at 100 but lets in other light, lower = good for filtering
     self.camera.awb_mode = "off"
     self.camera.awb_gains = (1.9, 1.9)
@@ -38,6 +39,75 @@ class Camera:
     return self.frame
   def stop(self):
     self.stopped = True
+    
+def getHSVImage(RGBImage, hue, sat, val):
+    HSVImage = cv2.cvtColor(RGBImage, cv2.COLOR_BGR2HSV)
+    return cv2.inRange(HSVImage, (hue[0], sat[0], val[0]), (hue[1], sat[1], val[1]))
+    
+def findContours(HSVImage):
+    im1, contours, hierarchy = cv2.findContours(HSVImage, mode = cv2.RETR_EXTERNAL, method = cv2.CHAIN_APPROX_SIMPLE)
+    return contours
+    
+def getContourImage(HSVImage, contours):
+    return cv2.drawContours(HSVImage, contours, -1, (255, 255, 0), 4)
+    
+def filterContours(contours, minWidth, maxWidth, minHeight, maxHeight):
+    filteredContours = []
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)    #duplicate calculation?
+        if w >= minWidth and w <= maxWidth and h >= minHeight and h <= maxHeight:
+            filteredContours.append(contour)
+    return filteredContours
+    
+def findBoundingRects(contours, minWidth, maxWidth, minHeight, maxHeight):
+    filteredRects = []
+    for contour in contours:
+        x,y,w,h = cv2.boundingRect(contour)
+        if w >= minWidth and w <= maxWidth and h >= minHeight and h <= maxHeight:
+            filteredRects.append((x,y,w,h))
+    return (filteredRects)
+    
+def distanceToTarget(targetWidth):
+    return targetWidthTimesFocalLength / targetWidth
+    
+videoStream = Camera().start()  # resolution found with    videoStream.camera.resolution[0]
+
+hue = [50, 150]
+sat = [30, 255]
+val = [30, 255]
+
+#constants found by measuring target
+targetWidthInPixels = 190
+targetDistanceInInches = 136
+targetWidthInInches = 20
+focalLength = (targetWidthInPixels*targetDistanceInInches)/targetWidthInInches
+
+#calculations that get re-used
+targetWidthTimesFocalLength = targetWidthInInches * focalLength
+
+while videoStream.read() is None: #wait until videostream isn't empty
+  time.sleep(0.1)
+
+while True:
+  currentFrame = videoStream.read()
+  filteredHSV = getHSVImage(currentFrame, hue, sat, val)
+  contours = findContours(filteredHSV)
+  filteredContours = filterContours(contours, 25, 600, 25, 400)
+  boundingRects = findBoundingRects(filteredContours, 25, 600, 25, 400)
+  distance = None
+  if len(boundingRects) > 0:
+    distance = distanceToTarget(boundingRects[0][2]) #[0][2] is first width in boundingRects array, later change to find actual target. first number is rectangle index, second is width
+  
+  print(distance)
+  print(boundingRects) 
+  
+  #cv2.imshow("HSV", filteredHSV)
+  cv2.imshow("Contours", getContourImage(filteredHSV, filteredContours))
+  #cv2.imshow("Frame", currentFrame)
+  
+  key = cv2.waitKey(1) & 0xFF
+  
+
     
 def getHSVImage(RGBImage, hue, sat, val):
     HSVImage = cv2.cvtColor(RGBImage, cv2.COLOR_BGR2HSV)
